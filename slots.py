2168bec950f9bc9ecc2f6429ec77d21a95e0ad22
Sh4dk <144249415+Sh4dk@users.noreply.github.com>
2024-01-17 14:36:52 +0330

    Update slots.py
    made it do less loops and searching
    a few output message changes

--- slots.py
+++ slots.py
@@ -1,36 +1,25 @@
 import random
 import itertools
 from collections import Counter
-
 high_tier = ['a','b']
 low_tier= ['c','d','e','f','g','h','i','j']
 emotes = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j']
 combinations = list(itertools.product(emotes, repeat=3))
-result = random.choice(combinations)
-amount = int(input('Enter the amount you want to gamble: '))
-
+result = random.choice(combinations)  
+amount = int(input('Place your bet: '))
 counter = Counter(result)
-
-count_to_multiplier = {
-   2: {'a': 25, 'b': 25},
-   3: {'a': 50, 'b': 50}
-}
-
-for count, multipliers in count_to_multiplier.items():
-   for emote, multiplier in multipliers.items():
-       if counter[emote] == count:
-           amount *= multiplier
-for emote in low_tier:
-   if counter[emote] == 2:
-       amount *= 1.5
-
-for emote in low_tier:
-   if counter[emote] == 3:
-       amount *= 2.5
-
 if len(set(result)) == len(result):
    amount *= 0
-
-print(result)
-print("Amount after betting: ", amount)
-
+for emote, count in counter.items():
+    if emote in high_tier:
+        if count == 2:
+            amount *= 25
+        elif count == 3:
+            amount *= 50
+    else:
+        if count == 2:
+            amount *= 1.5
+        elif count == 3:
+            amount *= 2.5
+print("After spinning the wheel you've got: ",dict(counter))
+print('Amount after betting: ', amount)
