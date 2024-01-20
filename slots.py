c0be008f84db986c56d9cf16c0afdae3456460af
Sh4dk <144249415+Sh4dk@users.noreply.github.com>
2024-01-20 20:31:17 +0330

    Update slots.py

--- slots.py
+++ slots.py
@@ -2,27 +2,39 @@ import random
 from collections import Counter
 
 emotes = ['a', 'b', 'c', 'd', 'e', 'f','g','h','i','j']
-result = random.choices(emotes, k=3)
-amount = int(input('Place your bet: '))
-counter = Counter(result)
-
+amount = 5000
+print('You start this game with 5000 points')
+print('------------------------------------')
 high_tier = {'a','b'}
 low_tier= {'c','d','e','f','g','h','i','j'}
 
-if len(set(result)) == len(result):
-   amount *= 0
-for idx, (emote, count) in enumerate(counter.items()):
-   if emote in high_tier:
-       if count == 2:
-           amount *= 25
-       elif count == 3:
-           amount *= 50
-   else:
-       if count == 2:
-           amount *= 1.50
-       elif count == 3:
-           amount *= 2.5
-
-print("After spinning the wheel you've got: ", dict(counter))
-print('Amount after betting: ', amount)
-
+while amount > 0:
+    bet = int(input('Place your bet: '))
+    if bet <= amount:
+        result = random.choices(emotes, k=3)
+        counter = Counter(result)
+        if len(set(result)) == len(result):
+            amount -= bet
+            bet *= 0
+        for idx, (emote, count) in enumerate(counter.items()):
+            if emote in high_tier:
+                if count == 2:
+                    bet *= 25
+                    amount += bet
+                elif count == 3:
+                    bet *= 50
+                    amount += bet
+            else:
+                if count == 2:
+                    bet *= 1.50
+                    amount += bet
+                elif count == 3:
+                    bet *= 2.5
+                    amount += bet
+        print("After pulling the lever you've got: ", dict(counter))
+        print("You win: ",bet,'points')
+        print('Your total points: ', amount)
+        print('----------------------------')
+    else:
+        print('You dont have', bet, 'points')
+    
